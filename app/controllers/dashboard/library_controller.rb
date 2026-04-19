82e20c2d18661d2ea8e96db939004ab49a11a40a
Wesley Sena <wesleysenan@gmail.com>
2026-04-18 23:52:00 -0300

    Feat: Add dashboard routes

---
+++ app/controllers/dashboard/library_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Dashboard
+  class LibraryController < ApplicationController
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  namespace :dashboard do
+    root to: "library#index"
+  end
+
   namespace :epic_games do
     get "library", to: "library#index"
   end
